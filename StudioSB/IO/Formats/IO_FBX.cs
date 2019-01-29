0c39b94fdf4462324db1976f86a36463bf46642f
ploaj_the_otter <ploaj_the_otter@yahoo.com>
2019-01-28 23:33:12 -0600

    adding missing dll to building

--- StudioSB/IO/Formats/IO_FBX.cs
+++ StudioSB/IO/Formats/IO_FBX.cs
@@ -1,8 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using StudioSB.IO.Models;
 using StudioSB.Scenes;
 using Fbx.ForgeFBX;
