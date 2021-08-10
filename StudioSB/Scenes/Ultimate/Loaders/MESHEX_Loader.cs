7e620dcbbf70147613b0472f74ee78dd5e2e7ebe
Carlos Aguilar <77519735+ssbucarlos@users.noreply.github.com>
2021-08-09 21:38:31 -0500

    .NUMSHEXB Export - BoundingSphere and TrueName changes. (#69)
    Fixes for all data and entry names

--- StudioSB/Scenes/Ultimate/Loaders/MESHEX_Loader.cs
+++ StudioSB/Scenes/Ultimate/Loaders/MESHEX_Loader.cs
@@ -47,8 +47,57 @@ namespace StudioSB.Scenes.Ultimate.Loaders
                     TrueName = GetTrueName(Name)
                 });
             }
-
+    
             Entries.Add(new EntryEx() { MeshExIndex = data });
+        }
+
+        /// <summary>
+        /// For each mesh in meshes, we will add an entry to 'Entries'. However, all meshes of the same name will get consolidated into
+        /// one single 'MeshData' entry that contains a new bounding sphere that encapsulates all the same-named meshes
+        /// </summary>
+        public void AddAllMeshData(List<SBUltimateMesh> Meshes)
+        {
+            Dictionary<string, List<SBUltimateMesh>> mesh_name_dict = new Dictionary<string, List<SBUltimateMesh>>();
+            foreach (var mesh in Meshes)
+            {
+                List<SBUltimateMesh> mesh_list = new List<SBUltimateMesh>();
+                if (mesh_name_dict.TryGetValue(mesh.Name, out mesh_list))
+                {
+                    mesh_list.Add(mesh);
+                }
+                else
+                {
+                    List<SBUltimateMesh> new_mesh_list = new List<SBUltimateMesh>();
+                    new_mesh_list.Add(mesh);
+                    mesh_name_dict.Add(mesh.Name, new_mesh_list);
+                }
+            }
+
+            foreach (var unique_mesh_name in mesh_name_dict.Keys)
+            {
+                List<SBUltimateMesh> mesh_list = mesh_name_dict[unique_mesh_name];
+                List<Vector3> grouped_vertices = new List<Vector3>();
+                foreach (var mesh in mesh_list)
+                {
+                    foreach (var vertex in mesh.Vertices)
+                    {
+                        grouped_vertices.Add(vertex.Position0);
+                    }
+                }
+                BoundingSphere group_sphere = new BoundingSphere(grouped_vertices);
+                MeshData.Add(new MeshEX()
+                {
+                    BoundingSphere = group_sphere,
+                    Name = unique_mesh_name,
+                    TrueName = GetTrueName(unique_mesh_name)
+                });   
+            }
+
+            foreach (var mesh in Meshes)
+            {
+                var data_index = MeshData.FindIndex(e => e.Name.Equals(mesh.Name));
+                Entries.Add(new EntryEx() { MeshExIndex = data_index });
+            }
         }
 
         /// <summary>
@@ -58,9 +107,10 @@ namespace StudioSB.Scenes.Ultimate.Loaders
         /// <returns></returns>
         private static string GetTrueName(string name)
         {
-            if (name.EndsWith("_VIS"))
-                name = name.Substring(0, name.IndexOf("_VIS"));
-
+            if (name.Contains("_VIS_"))
+                name = name.Substring(0, name.IndexOf("_VIS_"));
+            if (name.Contains("_O_"))
+                name = name.Substring(0, name.IndexOf("_O_"));
             if (name.EndsWith("Shape"))
                 name = name.Substring(0, name.IndexOf("Shape"));
 
